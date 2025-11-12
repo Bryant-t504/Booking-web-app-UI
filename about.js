1f2c94768d1fed58bc98c36a2acce65906dee4e8
Bryant <brianwasike504@gmail.com>
2025-11-12 12:05:08 +0300

    Fixing my background images

--- about.js
+++ about.js
@@ -11,3 +11,9 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+ const nav = document.querySelector(".nav");
+
+nav.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
